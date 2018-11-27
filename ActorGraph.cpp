173c30b3dff591af3a634616f1a7c9f4de97bb60
nozberkm <32474554+nozberkm@users.noreply.github.com>
2018-11-26 20:19:25 -0800

    Update ActorGraph.cpp

--- ActorGraph.cpp
+++ ActorGraph.cpp
@@ -1,10 +1,7 @@
 /*
  * ActorGraph.cpp
- * Author: <YOUR NAME HERE>
- * Date:   <DATE HERE>
+ * Author: Nabi Ozberkman
  *
- * This file is meant to exist as a container for starter code that you can use to read the input file format
- * defined in movie_casts.tsv. Feel free to modify any/all aspects as you wish.
  */
 
 #include <fstream>
